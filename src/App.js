5489d3457993267d3e48e9a7bc5f14d982179208
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-05 20:52:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navigation from './component/Navigation/Navigation';
+import Logo from './component/Logo/Logo';
+import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: 50}}>
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/component/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './facerecognitionlogo.png';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-3" options={{ max : 35 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner"> <img  src={brain} alt="Logo"/> </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
