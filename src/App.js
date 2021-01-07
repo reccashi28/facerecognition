5d68d848aa89279465e565e7d0027a07f584670e
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-07 22:28:30 +0100

    added tilt.js style

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import Navigation from './component/Navigation/Navigation';
 import Logo from './component/Logo/Logo';
 import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
 import Rank from './component/Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    line_linked: {
+      color: '#dce0e3'
+    }
+    // line_linked: {
+    //   shadow: {
+    //     enable: true,
+    //     color: "#3CA9D1",
+    //     blur: 5
+    //   }
+    // }
+  }
+};
 
-
-function App() {
+class App extends Component {
+render() {
   return (
     <div className="App" style={{padding: 50}}>
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
@@ -17,6 +40,7 @@ function App() {
       {/* <FaceRecognition /> */}
     </div>
   );
+}
 }
 
 export default App;
