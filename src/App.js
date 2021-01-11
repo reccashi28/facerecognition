346fce7cea58d3eb3650db1c43b8587df2d32153
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-11 21:10:02 +0100

    creating bounding box around the face

--- src/App.js
+++ src/App.js
@@ -32,8 +32,27 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: ''
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (e) => {
@@ -42,17 +61,13 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-
-      }
-    );
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+        .then(response =>  this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch( err => console.log(err)
+      );
   }
 
 render() {
@@ -65,7 +80,7 @@ render() {
       <ImageLinkForm 
         onInputChange ={this.onInputChange}
         onButtonSubmit={this.onButtonSubmit} />
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <FaceRecognition box = {this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   );
 }
