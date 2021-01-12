c964908a437ead324ed15893bbd1c3486f01fbba
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-12 07:30:21 +0100

    added facebox to the image

--- src/App.js
+++ src/App.js
@@ -33,13 +33,13 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: ''
+      box: '',
     }
   }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
--- src/component/FaceRecognition/FaceRecognition.js
+++ src/component/FaceRecognition/FaceRecognition.js
@@ -3,13 +3,14 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className="center mt5">
+        <div className="center ma">
             <div className="absolute mt2">
-                <img id="inputImage" src={imageUrl} alt="Pictures" width="500px" height="auto"/>
+                <img id="inputimage" src={imageUrl} alt="" width="500px" height="auto" />
+                <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>  </div>
             </div>
-            <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>  </div>
+            
         </div>
-    )
-}
+    );
+};
 
 export default FaceRecognition;
