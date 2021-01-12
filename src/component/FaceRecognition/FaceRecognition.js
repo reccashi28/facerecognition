94791b972626a94e1be82665740157b99745763b
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-12 07:37:51 +0100

    added margin to facerecog component

--- src/component/FaceRecognition/FaceRecognition.js
+++ src/component/FaceRecognition/FaceRecognition.js
@@ -3,7 +3,7 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className="center ma">
+        <div className="center mt3">
             <div className="absolute mt2">
                 <img id="inputimage" src={imageUrl} alt="" width="500px" height="auto" />
                 <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>  </div>
